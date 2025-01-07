ba44df7a0b65d0f14f2f5166f320378c7651a748
Keith Lewis <kclewis0614@gmail.com>
2025-01-07 11:36:57 -0600

    Pinned ruby version, added postgres to features
    fixed setup script path issue and added sanity check to avoid possible git issues

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/zsh
 sudo apt-get update && export DEBIAN_FRONTEND=noninteractive && sudo apt-get -y install --no-install-recommends libvips libvips-dev libvips-tools libpq-dev
-sudo mkdir /home/root/.config
-cd /home/root/.config
-git init .
-git remote add origin https://github.com/Greenwood-Cultural-Center/dotfiles.git
-git pull origin main
-cp /home/vscode/.config .
+sudo mkdir /root/.config
+cd /root/.config
+current_dir=$(pwd)
+if [ "$current_dir" = "/root/.config" ]; then 
+    git init .
+    git remote add origin https://github.com/Greenwood-Cultural-Center/dotfiles.git
+    git pull origin main
+    cp /home/vscode/.config .
+else 
+    echo "Not in /root/.config: skipping dotfiles setup for root: Please manually clone the dotfiles repo, https://github.com/Greenwood-Cultural-Center/dotfiles.git, to /root/.config"
+fi
 cd /workspaces/greenwood-kiosk
 brew install fzf
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-/root/.oh-my-zsh/custom}/themes/powerlevel10k
 bundle install
 yarn install

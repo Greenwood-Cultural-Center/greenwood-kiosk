ab06d528e4537993258cb545ab7647f6d1c97c5e
WoodGreenDev <joshua.williams51GW@outlook.com>
2025-03-28 10:14:43 -0500

    added make_feature method to json_controller, called compact on @ready_buildings to delete nil values that suddenly started popping up.

--- app/controllers/api/json_controller.rb
+++ app/controllers/api/json_controller.rb
@@ -11,7 +11,8 @@ module Api
       @buildings =  @@search_controller.search_buildings(params["search"],params["year"])
       @ready_buildings =[]
       
-      @buildings.each{|building|  @ready_buildings.append(make_building(building,params["year"])) } 
+      @buildings.each{|building|  @ready_buildings.append(make_feature(building,params["year"])) } 
+      @ready_buildings = @ready_buildings.compact
       @finished_json = build_json
       response.set_header('Access-Control-Allow-Origin', '*')
       render json: @finished_json
@@ -54,18 +55,139 @@ module Api
       
     end
 
-    
+    def make_feature(record,year)
+      def get_person(person)
+        person_narratives = []
+        person.narratives.each {|narrative| person_narratives.append({record: narrative,sources:narrative.sources,story:narrative.story})}
+        person_photos = []
+        person.photos.each {|photo| person_photos.append({record: photo,attatchment:photo.file_attachment,url:rails_blob_url(photo.file_attachment, only_path: true)}) }
+
+        
+        person_feature = {
+        "person": person,
+        "audios": person.audios,
+        "narratives": person_narratives,
+        "videos": person.videos,
+        "photos": person_photos
+
+
+        }
+      end
+      person_array_1910 =[]
+      person_array_1920 =[]
+      record.people_1910.each {|person| person_array_1910.append(get_person(person))}
+      record.people_1920.each {|person| person_array_1920.append(get_person(person))}
+
+      building_narratives = []
+      record.narratives.each {|narrative| building_narratives.append({record: narrative,sources:narrative.sources,story:narrative.story})}
+      building_photos = []
+      
+      if record.photos.empty? == false
+      record.photos.each {|photo| building_photos.append({record: photo,attatchment:photo.file_attachment,url:rails_blob_url(photo.file_attachment, only_path: true)}) }
+      end
+      if year == '1920'
+       
+       
+       
+            feature = {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": record.coordinates
+          },
+          "properties": {
+            "location_id": record.id,
+            "title": record.primary_street_address,
+            "building_addresses": record.addresses,
+            "building_audios": record.audios,
+            "building_narratives": building_narratives,
+            "building_videos": record.videos,
+            "building_photos": building_photos,
+            "description": record.full_street_address,
+            "rich_description": record.rich_text_description,
+            "1910": [],
+            "1920": record.census1920_records,
+            "1910_people": [],
+            "1920_people": person_array_1920
+          }
+        }
+     
+      elsif year == '1910'
+        feature = {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": record.coordinates
+      },
+      "properties": {
+        "location_id": record.id,
+        "title":  record.primary_street_address,
+        "building_addresses": record.addresses,
+        "building_audios": record.audios,
+        "building_narratives": building_narratives,
+        "building_videos": record.videos,
+        "building_photos": building_photos,
+        "description": record.full_street_address,
+        "rich_description": record.rich_text_description,
+        "1910": record.census1910_records,
+        "1920": [],
+        "1910_people": person_array_1910,
+        "1920_people": []
+        
+      }
+    } 
+
+      elsif year == 'Both'
+        feature = {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": record.coordinates
+      },
+      "properties": {
+        "location_id": record.id,
+        "title":  record.primary_street_address,
+        "building_addresses": record.addresses,
+        "building_audios": record.audios,
+        "building_narratives": building_narratives,
+        "building_videos": record.videos,
+        "building_photos": building_photos,
+        "description": record.full_street_address,
+        "rich_description": record.rich_text_description,
+        "1910": record.census1910_records,
+        "1920": record.census1920_records,
+        "1910_people": person_array_1910,
+        "1920_people": person_array_1920
+        
+      }
+    }
+      end 
+      
+      
+     
+      if feature[:properties][:"1920"].empty? && year =='1920'
+        
+        return 
+     end
+
+     if feature[:properties][:"1910"].empty? && year =='1910'
+      
+      return 
+   end
+      
+      return feature
+    end
 
     private def make_building(record,year)
       
-        feature = @@search_controller.make_feature(record,year)
+        #feature = @@search_controller.make_feature(record,year)
        
       
       
      
       
       
-      return feature
+      #return feature
     end
 
     
